39369054f315505c870873018650a2b3289dea2f
Sebas Agudelo <shebisx1@hotmail.com>
2018-02-10 20:01:46 -0500

    Init Spring base code

---
+++ Protege/GameOfThronesOntology/src/main/java/com/GoTOnt/GameOfThronesOntology/GameOfThronesOntologyApplication.java
@@ -0,0 +1,12 @@
+package com.GoTOnt.GameOfThronesOntology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThronesOntologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfThronesOntologyApplication.class, args);
+	}
+}
